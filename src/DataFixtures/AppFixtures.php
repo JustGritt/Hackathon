110e694220f4e1b57e29f86daa823ca6a78b2b91
Charles <charles258@hotmail.fr>
2023-03-29 20:26:56 +0200

    Add User fixture, admin and modo user

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    // ...
+    public function load(ObjectManager $manager)
+    {
+    
+        $user = new User();
+        $user->setFirstname('admin');
+        $user->setLastname('admin');
+        $user->setEmail('admin@admin.fr');
+        $user->setIsVerified(true);
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($user, 'esgi1234');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $modo = new User();
+        $modo->setFirstname('modo');
+        $modo->setLastname('modo');
+        $modo->setEmail('modo@modo.fr');
+        $modo->setIsVerified(true);
+        $modo->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($modo, 'esgi1234');
+        $modo->setPassword($password);
+
+        $manager->persist($modo);
+        $manager->flush();
+
+        
+    }
+}
