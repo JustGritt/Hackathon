38e26a66df87796c29d8d0146d7b82c6f01b850f
Charles <charles258@hotmail.fr>
2023-03-31 13:34:15 +0200

    Hotfix migrations

--- migrations/Version20230330115925.php
+++ migrations/Version20230330115925.php
@@ -20,11 +20,9 @@ final class Version20230330115925 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE answer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE answer (id INT NOT NULL, question_id INT NOT NULL, content VARCHAR(255) NOT NULL, is_response BOOLEAN NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_DADD4A251E27F6BF ON answer (question_id)');
-        $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE question DROP is_response');
+        //$this->addSql('CREATE SEQUENCE answer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        //$this->addSql('CREATE INDEX IDX_DADD4A251E27F6BF ON answer (question_id)');
+        //$this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
