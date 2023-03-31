b680567aa281de20303dee240f589881c01f369a
Charles <charles258@hotmail.fr>
2023-03-31 13:11:33 +0200

    Hotfix migrations

--- migrations/Version20230330134803.php
+++ migrations/Version20230330134803.php
@@ -20,8 +20,6 @@ final class Version20230330134803 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE commentaire_video_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE hk_video_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('ALTER TABLE commentaire_video ALTER user_id_id DROP NOT NULL');
         $this->addSql('ALTER TABLE commentaire_video ALTER video_id_id DROP NOT NULL');
     }
